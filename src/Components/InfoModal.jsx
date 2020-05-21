0f20b9ee7f05f80073c0923d39e8bac557c47b95
Peter Choi <ptrchoi@gmail.com>
2020-05-21 13:10:36 -0500

    trimmed down About content

--- src/Components/InfoModal.jsx
+++ src/Components/InfoModal.jsx
@@ -10,33 +10,10 @@ function InfoModal(props) {
 				<h1>About</h1>
 				<div className="info-content">
 					<p>
-						A clean mobile + desktop React app that generates inspirational quotes over fullscreen,
-						high-resolution images. Fully responsive, fluidly scaling images and text, with content pulling
-						from several popular API's. Users can choose from a variety of photo themes, pause on the
-						current content (ie. for a screengrab), or tweet out the current quote.
-					</p>
-					<p>
-						<h4>Technologies:</h4>
 						<ul>
-							<li>Javascript | React | jQuery | SCSS | Firebase | Node.js | Parcel</li>
-						</ul>
-					</p>
-					<p>
-						<h4>Features and UX:</h4>
-						<ul>
-							<li>Quotes generated from the Forismatic API.</li>
-							<li>Quote positioning is also dynamcially randomized within the viewport.</li>
-							<li>
-								Responsive design with full-screen, fluid-scaling background images and responsive
-								font-size scaling (ie. no harsh breakpoints), with transitional animations between
-								content cycling.
-							</li>
-							<li>Themed images generated from Unsplash's image collections.</li>
-							<li>Animated buttons, tweet sharing, information modal overlay.</li>
-							<li>
-								Code demonstrates functional programming with higher-order functions, advanced ES6
-								features, and modular programming with React and SCSS components.
-							</li>
+							<li>Quotes from Forismatic.</li>
+							<li>Images from Unsplash.</li>
+							<li>Designed and Developed by Peter Choi (ptrchoi.com)</li>
 						</ul>
 					</p>
 				</div>
